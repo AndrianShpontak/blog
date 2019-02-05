9c98e5df054bf9573846ef4bb49e72826e274464
Andriana Ihnatyshch <andriana@pettersonapps.com>
2019-02-05 10:31:15 +0200

    route for message

---
+++ routes/messageRoom.js
@@ -0,0 +1,10 @@
+const MessageRoomHandler = require('../handlers/messageRoom');
+const express = require('express');
+const router = express.Router();
+const messageRoomHandler = new MessageRoomHandler();
+
+router.post('/createRoom', messageRoomHandler.createOrGetChatRoom);
+router.post('/readMessage', messageRoomHandler.readMessage);
+router.post('/', messageRoomHandler.createMessage);
+
+module.exports = router;
